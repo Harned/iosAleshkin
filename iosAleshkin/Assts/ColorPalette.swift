a1955c15781392dbc35efa0de25b608ac8faab1e
Harned <aleshkindima25@gmail.com>
2024-02-24 14:50:56 +0600

    ColorPalette

---
+++ iosAleshkin/Assts/ColorPalette.swift
@@ -0,0 +1,20 @@
+//
+//  ColorPalette.swift
+//  iosAleshkin
+//
+//  Created by Nes on 24.02.2024.
+//
+
+import SwiftUI
+import Foundation
+
+
+struct BaseColor {
+    let textPrimaryDark = Color("TextPrimaryDark")
+    let textSecondaryDark = Color("TextSecondaryDark")
+    let textPrimaryLight = Color("TextPrimaryLight")
+    let textSecondaryLight = Color("TextSecondaryLight")
+    let themePrimary = Color("ThemePrimary")
+    let themeSecondary = Color("ThemeSecondary")
+    let background = Color("Background")
+}
---
+++ iosAleshkin/views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  iosAleshkin
+//
+//  Created by Nes on 24.02.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
