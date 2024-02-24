e409b23f650cabbec7e7152685b2f62093de75f5
kaiscend <esnaum38@gmail.com>
2024-02-24 12:48:11 +0600

    Initial Commit

---
+++ iosAleshkin/iosAleshkinApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAleshkinApp.swift
+//  iosAleshkin
+//
+//  Created by Nes on 24.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct iosAleshkinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
